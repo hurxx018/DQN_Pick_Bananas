fc0c188debeb0f2f77ccc581f472e0fdb560a6dc
Kwang Ho Hur <hurxx018@gmail.com>
2020-08-05 08:54:29 -0500

    docs: Add a document to class QNetworks
    Describe the elements of QNetworks.

--- model.py
+++ model.py
@@ -4,8 +4,29 @@ import torch.nn.functional as F
 
 
 class QNetworks(nn.Module):
+    """ Actor Model.
+        This model is a function approximation of action-values. 
+        The networks is a mapping from a state to a list of action values, so
+        the agent uses its own policy such as greedy- or epsilon-greedy policies.
+        The model is implemented as a fully-connected multilayer networks. 
 
-    def __init__(self):
+
+        Arguments
+        ---------
+        state_size : int
+            Number of states of the environment.
+        action_size : int
+            Number of actions that the agent can take.
+        seed : int
+            Seed for a random number generator.
+
+    """
+    def __init__(
+        self,
+        state_size,
+        action_size,
+        seed
+        ):
         super(QNetworks, self).__init__()
 
 
