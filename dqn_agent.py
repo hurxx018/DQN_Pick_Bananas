92852b9fcacc4998a083650fbecfd4cd280b87c5
Kwang Ho Hur <hurxx018@gmail.com>
2020-08-05 10:12:11 -0500

    feat: Update class DQNAgent
    Define instance methods of DQNAgent; act, step, learn, soft_update.

--- dqn_agent.py
+++ dqn_agent.py
@@ -2,6 +2,8 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 
 
@@ -10,3 +12,30 @@ class DQNAgent():
     def __init__(self):
         super(DQNAgent, self).__init__()
 
+
+    def step(
+        self,
+        state,
+        action,
+        reward,
+        next_state, 
+        done
+        ):
+        pass
+
+    def act(
+        self,
+        state,
+        epsilon = 0. 
+        ):
+        pass
+
+    def learn(
+        self
+        ):
+        pass
+
+    def soft_update(
+        self
+        ):
+        pass
