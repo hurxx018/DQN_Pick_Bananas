eac348de35e87bde9bd5edc73f000630977ab6a1
Kwang Ho Hur <hurxx018@gmail.com>
2020-08-05 08:38:23 -0500

    feat: Initiate the definition of class QNetworks
    The class QNetworks will be implemented as a fully-connected
    multi-layer networks.

---
+++ model.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class QNetworks(nn.Module):
+
+    def __init__(self):
+        super(QNetworks, self).__init__()
+
+
+
+    def forward(
+        self, 
+        x
+        ):
+        pass
