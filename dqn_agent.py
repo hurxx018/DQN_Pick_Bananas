92d354b22082f3bae6d5f232456cbfbc6ffba408
Kwang Ho Hur <hurxx018@gmail.com>
2020-08-05 10:49:57 -0500

    feat: Implement __init__ of DQNAgent
    DQNAgent is instantiated with three variables; state_size,
    action_size, and seed. It uses two instances of QNetworks.
    The local QNetworks is used for the continuous learning of the
    agent.
    The targe QNetworks is a kind of the approximate to the true
    action-value function of the environment.
    The learning procedure uses the mean-square-error loss to evaluate
    the policy.
    The local QNetworks' parameters are updated with an Adam optimizer.
    The target QNetworks is updated every UPDATE_EVERY.
    ReplayBuffer is stored in an instance variable memory.

--- dqn_agent.py
+++ dqn_agent.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-
+from model import QNetworks, weights_init_normal
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 64         # minibatch size
@@ -26,6 +26,31 @@ class DQNAgent(object):
         ):
         super(DQNAgent, self).__init__()
 
+        # Initialize instance variables
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = seed
+
+        # Q-Networks
+        self.qnetworks_local = QNetworks(state_size, action_size, seed).to(device)
+        self.qnetworks_local.apply(weights_init_normal)
+        self.qnetworks_target = QNetworks(state_size, action_size, seed).to(device)
+        self.qnetworks_target.apply(weights_init_normal)
+
+        # Freeze gradients for parameters of qnetworks_target
+        # that are not used for the learning.
+        for param in self.qnetworks_target.parameters():
+            param.requires_grad_(False)
+
+        # Initialize elements for the training
+        self.criterion = nn.MSELoss()
+        self.optimizer = optim.Adam(self.qnetworks_local.parameters(), lr=LR)
+
+        # Replay memory
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
+
+        # Initialize time step (for updating every UPDATE_EVERY steps)
+        self.t_step = 0
 
     def step(
         self,
