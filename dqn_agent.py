52ba582dfaadc03622e8de2083d1f9083919d16e
Kwang Ho Hur <hurxx018@gmail.com>
2020-08-05 10:33:35 -0500

    feat: Define class ReplayBuffer
    Begin to implement class ReplayBuffer.

--- dqn_agent.py
+++ dqn_agent.py
@@ -7,9 +7,23 @@ import torch.optim as optim
 
 
 
-class DQNAgent():
+BUFFER_SIZE = int(1e5)  # replay buffer size
+BATCH_SIZE = 64         # minibatch size
+GAMMA = 0.99            # discount factor
+TAU = 1e-3              # for soft update of target parameters
+LR = 5e-3               # learning rate 
+UPDATE_EVERY = 4        # how often to update the network
 
-    def __init__(self):
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class DQNAgent(object):
+
+    def __init__(
+        self,
+        state_size,
+        action_size,
+        seed
+        ):
         super(DQNAgent, self).__init__()
 
 
@@ -38,4 +52,34 @@ class DQNAgent():
     def soft_update(
         self
         ):
+        pass
+
+
+class ReplayBuffer(object):
+
+    def __init__(
+        self,
+        action_size,
+        buffer_size,
+        batch_size, 
+        seed
+        ):
+        pass
+
+
+    def add(
+        self,
+        state,
+        action, reward,
+        next_state, 
+        done
+        ):
+        pass
+
+    def sample(
+        self
+        ):
+        pass
+
+    def __len__(self):
         pass
